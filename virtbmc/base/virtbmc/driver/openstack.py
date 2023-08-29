ae35984d369d0ee1c2cfb820944d3ef40e267612
chramb <79099832+chramb@users.noreply.github.com>
2023-08-29 17:53:31 +0200

    test openstackbmc v0.5: power on/off

--- virtbmc/base/virtbmc/driver/openstack.py
+++ virtbmc/base/virtbmc/driver/openstack.py
@@ -1,13 +1,43 @@
 from __future__ import annotations
 
+"""
+    def _ensure_status(self, target_status: status, interval: float = 5) -> None:
+        # TODO: somehow handle both active and shutoff if changed meant to run in background
+        while self.server.status != target_status and self._started:
+            # refresh server instance
+            with openstack.connect(self.cloud) as conn:
+                server: Union[Server, None] = conn.compute.get_server(self.server)
+                if server is None:
+                    raise Exception("server stopped existing in meantime")
+                if target_status == "SHUTOFF":
+                    conn.compute.stop_server(server)
+                if target_status == "ACTIVE":
+                    conn.compute.stop_server(server)
+
+            time.sleep(interval)
+        return
+    def power_off(self):
+        try:
+            if (
+                self.server.vm_state == "stopped"
+                or self.server.task_state == "powering-off"
+            ):
+                return
+            if self.server.task_state is None:
+                self.server.stop(self.conn)
+                self.server.task_state = "powering-off"
+
+            return 0xC1
+"""
 import logging
-import time
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Optional
 
 import openstack
+import openstack.exceptions
 from openstack.compute.v2.server import Server
 from openstack.connection import Connection
+from requests import session
 from virtbmc.driver.base import BaseBMC
 
 if TYPE_CHECKING:
@@ -24,18 +54,23 @@ if TYPE_CHECKING:
 log = logging.getLogger("virtbmc.driver.dummy")
 # connection_args: Dict[str, str] = {"app_name": "VirtBMC", "app_version": "0.4"}
 
+# Command failed and can be retried
+IPMI_COMMAND_NODE_BUSY = 0xC0
+# Invalid data field in request
+IPMI_INVALID_DATA = 0xCC
+
 
 @dataclass
 class OpenStackBMC(BaseBMC):
     name: str
-    cloud: Optional[str] = "__undefined__"
+    cloud: Optional[str] = None
     driver: str = field(default="openstack", init=False)
     server: Server = field(init=False, repr=False)
-    _target: str = field(default="none", init=False, repr=False)
+    conn: Connection = field(init=False, repr=False)
+    _target: str = field(init=False, repr=False)
 
     def __post_init__(self) -> None:
-        if self.cloud == "__undefined__":
-            self.cloud = None
+        conn = openstack.connect(cloud=self.cloud)
         return super().__post_init__()
 
     def start(self):
@@ -47,25 +82,9 @@ class OpenStackBMC(BaseBMC):
         print("started")
         super().start()
 
-    def _check_server_missing(self, server: Optional[Server]):
-        if server is None:
-            raise Exception("server stopped existing in meantime")
-
-    def _ensure_status(self, target_status: status, interval: float = 5) -> None:
-        # TODO: somehow handle both active and shutoff if changed
-        while self.server.status != target_status and self._started:
-            # refresh server instance
-            with openstack.connect(self.cloud) as conn:
-                server: Union[Server, None] = conn.compute.get_server(self.server)
-                if server is None:
-                    raise Exception("server stopped existing in meantime")
-                if target_status == "SHUTOFF":
-                    conn.compute.stop_server(server)
-                if target_status == "ACTIVE":
-                    conn.compute.stop_server(server)
-
-            time.sleep(interval)
-        return
+    def stop(self):
+        self._started = False
+        self.conn.close()
 
     # BMC Operations
     # ref: 28.3 Chassis Control Command: https://www.intel.com/content/dam/www/public/us/en/documents/specification-updates/ipmi-intelligent-platform-mgt-interface-spec-2nd-gen-v2-0-spec-update.pdf
@@ -74,39 +93,51 @@ class OpenStackBMC(BaseBMC):
         return 0
 
     def is_active(self):
-        with openstack.connect(self.cloud) as conn:
-            conn.compute.get_server(self.server)
+        with openstack.connect(session=self.conn) as conn:
+            self.server = conn.compute.get_server(self.server)
+            if self.server is None:
+                raise Exception("server stopped existing in meantime")
             return self.server.status == "ACTIVE"
 
     def get_power_state(self):
-        with openstack.connect(self.cloud) as conn:
-            self.server = conn.compute.get_server(self.server)
-            #     "off": 0,
-            #     "on": 1,
-            self._check_server_missing(self.server)
-
-            return "on" if self.server.status == "ACTIVE" else "off"
+        #     "off": 0,
+        #     "on": 1,
+        return "on" if self.is_active() else "off"
 
     def power_off(self):
+        print("called power-off")
         # this should be power down without waiting for clean shutdown
-        with openstack.connect(self.cloud) as conn:
-            if self._target != "off":
-                self._target = "off"
-                conn.compute.stop_server(self.server)
-        return
+        if self.server.status == "SHUTOFF" or self.server.task_state == "powering-off":
+            return
+
+        if self.server.status == "ACTIVE":
+            self.server.stop(self.conn.compute)
+            self.server.task_state = "powering-off"
+            return
+
+        self.server = self.conn.compute.get_server(self.server)
+
+        return IPMI_COMMAND_NODE_BUSY
 
     def power_on(self):
-        with openstack.connect(self.cloud) as conn:
-            self.server: Server = conn.compute.get_server(self.server)
-            if self._target != "on":
-                self._target = "on"
-                conn.compute.start_server(self.server)
-        return
+        print("called power on")
+        if self.server.status == "ACTIVE" or self.server.task_state == "powering-on":
+            return
+
+        if self.server.status in ["SHUTOFF", "STOPPED"]:
+            self.server.stop(self.conn.compute)
+            self.server.task_state = "powering-on"
+            return
+
+        self.server = self.conn.compute.get_server(self.server)
+
+        return IPMI_COMMAND_NODE_BUSY
 
     def power_reset(self):
         with openstack.connect(self.cloud) as conn:
             conn.compute.reboot_server(self.server, reboot_type="SOFT")
 
     def power_shutdown(self):
+        print("called shutdown")
         # Should attempt a clean shutdown but openstack doesn't know the difference I think
         self.power_off()
