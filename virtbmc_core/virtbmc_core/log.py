a9b57154506633f07232cfd42b42fdec358218fd
chramb <79099832+chramb@users.noreply.github.com>
2023-09-02 00:18:52 +0200

    feat(core): add common logger log formatting function

---
+++ virtbmc_core/virtbmc_core/log.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import logging
+import os
+
+# BRIGHT_BLACK = "\33[30;1m"
+# BRIGHT_RED = "\33[31;1m"
+# BRIGHT_GREEN = "\33[32;1m"
+# BRIGHT_YELLOW = "\33[33;1m"
+# BRIGHT_BLUE = "\33[34;1m"
+# BRIGHT_MAGENTA = "\33[35;1m"
+# BRIGHT_CYAN = "\33[36;1m"
+# BRIGHT_WHITE = "\33[37;1m"
+# BLACK = "\33[30m"
+RED = "\33[31m"
+# GREEN = "\33[32m"
+YELLOW = "\33[33m"
+BLUE = "\33[34m"
+# MAGENTA = "\33[35m"
+# CYAN = "\33[36m"
+# WHITE = "\33[37m"
+RESET = "\33[0m"
+BOLD = "\33[1m]"
+# UNDERLINE = "\33[4m"
+# REVERSED = "\33[7m"
+
+
+def log_format(
+    logger: logging.Logger,
+    message_format: str = "{asctime} {levelname}: {message}",
+    date_format: str = "%Y-%m-%d %H:%M:%S",
+) -> None:
+    if os.isatty(2) and not os.environ.get("NO_COLOR", False):
+        LEVEL_FORMAT: dict[int, str] = {
+            logging.DEBUG: f"{BLUE}{message_format}{RESET}",
+            logging.INFO: f"{message_format}",
+            logging.WARNING: f"{YELLOW}{message_format}{RESET}",
+            logging.ERROR: f"{RED}{message_format}{RESET}",
+            logging.CRITICAL: f"{BOLD}{RED}{message_format}{RESET}",
+        }
+
+        class ColorFormatter(logging.Formatter):
+            def format(self, record) -> str:
+                _format: str = LEVEL_FORMAT[record.levelno]
+                _formatter = logging.Formatter(
+                    fmt=_format, style="{", datefmt=date_format
+                )
+                return _formatter.format(record)
+
+        formatter = ColorFormatter()
+    else:
+        formatter = logging.Formatter(
+            fmt=message_format, style="{", datefmt=date_format
+        )
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
