e97ab2a515ef1993a996449196b505e0c3e9f12f
chramb <79099832+chramb@users.noreply.github.com>
2023-09-13 12:13:03 +0200

    fix(openstack): cli: add --os-debug flag
    enables openstack.enable_logging(debug=True)

--- virtbmc_openstack/virtbmc_openstack/__main__.py
+++ virtbmc_openstack/virtbmc_openstack/__main__.py
@@ -23,7 +23,8 @@ def main(args: Optional[Sequence[str]] = None) -> int:
     parser = ArgumentParser(prog="python -m virtbmc_openstack", add_help=False, formatter_class=lambda prog: HelpFormatter(prog, max_help_position=36))  # noqa: E501
     parser.add_argument("-h", "--help", action="help", help="Show this help message and exit")  # noqa: E501
     parser.add_argument("-V","--version", action="version",version=importlib.metadata.version(__package__), help="Show program's version number and exit")  # noqa: E501
-    parser.add_argument("-d","--debug", action="store_true", help="enable logging debug information to console")  # noqa: E501
+    parser.add_argument("-d","--debug", action="store_true", help="Enable logging debug information to console")  # noqa: E501
+    parser.add_argument("--os-debug", action="store_true", help="Enable logging debug information for openstacksdk")  # noqa: E501
 
     parser, group = bmc_options(parser)
     group.add_argument("--mode", action="store", default="safe", choices=("safe", "unsafe", "cached", "simple"), help="Chose api communication strategy")  # noqa: E501
@@ -34,9 +35,13 @@ def main(args: Optional[Sequence[str]] = None) -> int:
     parsed: Namespace = parser.parse_args(args=args)
     log_format(log)
 
+    if parsed.os_debug:
+        openstack.enable_logging(debug=True)
+
     if parsed.debug:
         log.setLevel(logging.DEBUG)
         log.debug("Enabled debugging from cli")
+
     bmc = None
     try:
         bmc = OpenStackBMC(
