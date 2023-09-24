87630595db9b1260b868d074465f4ebe37a77375
chramb <79099832+chramb@users.noreply.github.com>
2023-09-24 21:17:45 +0200

    refactor(base): rename import cmt -> ctl

--- virtbmc/virtbmc/api/rpc/server.py
+++ virtbmc/virtbmc/api/rpc/server.py
@@ -2,16 +2,16 @@ from __future__ import annotations
 
 from multiprocessing.managers import BaseManager
 
-import virtbmc.control as cmd
+import virtbmc.control as ctl
 
 
 class Server(BaseManager):
     pass
 
 
-Server.register("bmc_create", cmd.create)
-Server.register("bmc_delete", cmd.delete)
-Server.register("bmc_start", cmd.start)
-Server.register("bmc_stop", cmd.stop)
-Server.register("bmc_get", cmd.get)
-Server.register("bmc_get_all", cmd.get_all)
+Server.register("bmc_create", ctl.create)
+Server.register("bmc_delete", ctl.delete)
+Server.register("bmc_start", ctl.start)
+Server.register("bmc_stop", ctl.stop)
+Server.register("bmc_get", ctl.get)
+Server.register("bmc_get_all", ctl.get_all)
